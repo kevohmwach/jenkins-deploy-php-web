362154d8a0487a468816be1a27d4141cd8df5231
kevohmwach <50871815+kevohmwach@users.noreply.github.com>
2025-01-20 12:53:19 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test sites</title>
+    
+</head>
+<body>
+    <h1>Devops Test site</h1>
+</body>
+</html>
